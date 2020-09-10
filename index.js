836d526d6a7b20c37c37cf6c6fa7e4e3a635dae4
odemaster <60938683+odemaster@users.noreply.github.com>
2020-09-10 22:06:29 +0800

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-let  name = 'Warning! this webpage contains virus, proceed at your own risk , "everything you about to see is full of bullshit" hahahaha charot lang  -Dexter';
-console.log(name);
-alert(name);
-alert('Sure ka na ba dyan?');
-alert('Sanaol sure na.... osige....');
