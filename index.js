efcfdcf23a6cfcf7de848da8e68a52e3a060e928
odemaster <60938683+odemaster@users.noreply.github.com>
2020-09-10 19:11:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let  name = 'Warning! this webpage contains virus, proceed at your own risk , "everything you about to see is full of bullshit" hahahaha charot lang  -Dexter';
+console.log(name);
+alert(name);
+alert('Sure ka na ba dyan?');
+alert('Sanaol sure na.... osige....');
